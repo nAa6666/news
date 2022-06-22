79b2c213f4cc570af38d2f5ca1df1d6c951eed6e
Vitaliy Gromovoi <vetaldxdx@gmail.com>
2022-06-22 18:04:55 +0300

    'test'

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Sign in</title>
-    <link rel="stylesheet" href="{{ mix('styles/compiled/admin/style.min.css') }}">
+    <link rel="stylesheet" href="{{mix('styles/compiled/admin/style.min.css')}}">
 </head>
 <body>
 <div id="login">
